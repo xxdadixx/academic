02052b9f917e47ada2b1392c4c5a41b3aa1cf76d
xxdadixx <sanshop4@hotmail.com>
2022-07-27 01:46:12 +0700

    update user.sql

--- users.sql
+++ users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2022 at 10:58 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Jul 26, 2022 at 08:45 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,10 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `phone_number` varchar(255) NOT NULL,
   `id_card_number` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'default-image.png',
+  `birthday` date NOT NULL,
+  `weight` varchar(255) NOT NULL DEFAULT '-',
+  `height` varchar(255) NOT NULL DEFAULT '-',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -44,9 +48,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `phone_number`, `id_card_number`, `created_at`, `updated_at`) VALUES
-(6, 'Ittikorn', 'Sirivejaband', 'xxdadixx', 'sanshop4@hotmail.com', '$2y$10$z9Wc2uxB88swnUzlBpeDku3zCbsv6EB.rC0lbSmwBFtvlAvZRrCLe', '0000000000', '0000000000000', '2022-05-10 02:58:14', '2022-05-10 02:58:14'),
-(19, 'asd', 'asd', 'xxdadixx1', 'sanshop3@hotmail.com', '$2y$10$1VJsOlZ05KxXu5cDGtw5luz/2zeth.928pbLHQEVu2vUvtZrG/7r.', '0863992217', '0000000000000', '2022-05-10 03:34:00', '2022-05-10 03:34:00');
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `phone_number`, `id_card_number`, `image`, `birthday`, `weight`, `height`, `created_at`, `updated_at`) VALUES
+(6, 'Ittikorn', 'Sirivejaband', 'xxdadixx', 'sanshop4@hotmail.com', '$2y$10$VtI3vj3yTe1iwtRPB.ZFAOqTKPFuW8qX/V5FsGB3sv405SmCFivLO', '0910711894', '1509966122528', 'xxdadixx62850a83c12793.88386579.jpg', '2001-03-08', '50', '174', '2022-05-10 02:58:14', '2022-05-10 02:58:14'),
+(19, 'asd', 'asd', 'xxdadixx1', 'sanshop3@hotmail.com', '$2y$10$1VJsOlZ05KxXu5cDGtw5luz/2zeth.928pbLHQEVu2vUvtZrG/7r.', '0863992217', '1509966122527', 'default-image.png', '2014-07-08', '-', '-', '2022-05-10 03:34:00', '2022-05-10 03:34:00');
 
 --
 -- Indexes for dumped tables
